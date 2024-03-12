26340b3bb0a63ced7ac4079ebe469ce4deb49e10
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-03-12 10:31:12 -0600

    Agregando Arrays como archivos bases para mistrar en la lista de productos

--- src/app/components/list-products/list-products.component.ts
+++ src/app/components/list-products/list-products.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../interfaces/product';
 
 @Component({
   selector: 'app-list-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list-products.component.html',
   styleUrl: './list-products.component.css'
 })
-export class ListProductsComponent {
+export class ListProductsComponent implements OnInit {
+  listProducts: Product[] = [
+    {name : 'Queso', price: 100, description: 'Queso Amarillo', stock: 10},
+    {name : 'Leche', price: 50, description: 'Leche Descremada', stock: 20},
+  ]
+  
+  constructor() {}
+
+  ngOnInit(): void {}
 
 }
