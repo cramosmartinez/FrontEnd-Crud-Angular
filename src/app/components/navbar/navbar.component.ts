1e64039e459c3ae1d54b268f4101251759c52108
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-03-12 10:09:41 -0600

    Se agrego navbar, componentes y servicios

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {}
 }
